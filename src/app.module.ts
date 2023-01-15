59e9f8dd39cb5991c86053a75a958e2c631496be
AndresJ <joel.coarita@mediabyte.com.pe>
2023-01-14 19:08:51 -0500

    inicio del proyecto teslo shop nest

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductsModule,
+
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
